257640a5b9a09bd6e54e927cba02b68e146fbb56
Fayzulla Rahmatullayev <fayzullarakhmatullayev@gmail.com>
2023-02-20 18:34:48 +0500

    updated

---
+++ dist/js/global.js
@@ -0,0 +1,427 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"global": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./src/js/global.js","vendor"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/global.js":
+/*!**************************!*\
+  !*** ./src/js/global.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var gsap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! gsap */ "./node_modules/gsap/index.js");
+/* harmony import */ var gsap_ScrollTrigger__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! gsap/ScrollTrigger */ "./node_modules/gsap/ScrollTrigger.js");
+/* harmony import */ var _resize__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./resize */ "./src/js/resize.js");
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+
+
+
+gsap__WEBPACK_IMPORTED_MODULE_0__["default"].registerPlugin(gsap_ScrollTrigger__WEBPACK_IMPORTED_MODULE_1__["ScrollTrigger"]);
+var firstScreen = /*#__PURE__*/function () {
+  function FirstScreen() {
+    var isShowTentacles = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+    _classCallCheck(this, FirstScreen);
+    this.cursorAnimationParams = {
+      count: 100,
+      colors: ["#FF4978", "#430A66"],
+      radius: 40,
+      speed: 1,
+      lifetime: 150,
+      element: "cursor"
+    };
+    this.circles = [];
+    this.isShowTentacles = isShowTentacles;
+  }
+  _createClass(FirstScreen, [{
+    key: "setCanvasSize",
+    value: function setCanvasSize() {
+      return this.cursorAnimationParams;
+    }
+  }, {
+    key: "createCanvas",
+    value: function createCanvas() {
+      var _this = this;
+      return new Promise(function (resolve, reject) {
+        if (document.getElementById(_this.cursorAnimationParams.element)) {
+          resolve({
+            canvas: _this.canvas = document.getElementById(_this.cursorAnimationParams.element),
+            ctx: _this.canvas ? _this.ctx = _this.canvas.getContext("2d") : null,
+            width: _this.width = _this.canvas.width = document.querySelector(".first-screen").clientWidth,
+            height: _this.height = _this.canvas.height = document.querySelector(".first-screen").clientHeight
+          });
+        } else {
+          reject(new Error("params.element is not defined"));
+        }
+        _this.gradient = _this.ctx.createLinearGradient(0, 0, window.innerWidth, 0);
+        for (var i = 0; i < _this.cursorAnimationParams.colors.length; i++) {
+          _this.gradient.addColorStop(i, _this.cursorAnimationParams.colors[i]);
+        }
+      });
+    }
+  }, {
+    key: "updateCanvasSize",
+    value: function updateCanvasSize() {
+      var _this2 = this;
+      return new Promise(function (resolve, reject) {
+        if (_this2.canvas && _this2.ctx) {
+          return resolve({
+            width: _this2.width = _this2.canvas.width = document.querySelector(".first-screen").clientWidth,
+            height: _this2.height = _this2.canvas.height = document.querySelector(".first-screen").clientHeight
+          });
+        }
+      });
+    }
+  }, {
+    key: "pushCircleObject",
+    value: function pushCircleObject(x, y) {
+      var circle = {
+        x: x,
+        y: y,
+        lifetime: this.cursorAnimationParams.radius,
+        radius: this.cursorAnimationParams.radius
+      };
+      this.circles.push(circle);
+    }
+  }, {
+    key: "animate",
+    value: function animate(_ref) {
+      var x = _ref.x,
+        y = _ref.y;
+      if (!this.canvas) return;
+      this.pushCircleObject(x, y);
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.fillStyle = this.gradient;
+      this.ctx.strokeStyle = this.gradient;
+      // this.ctx.drawImage(this.cursorAnimationParams.shape, 0, 0, this.canvas.width, this.canvas.height);
+      for (var i = 1; i < this.circles.length; i++) {
+        this.circles[i].lifetime -= this.cursorAnimationParams.speed;
+        if (this.circles[i].lifetime <= 0) {
+          this.circles.splice(i, 1);
+        }
+        if (this.circles[i] && this.circles[i + 1]) {
+          this.ctx.lineWidth = this.circles[i].radius * 2;
+          this.ctx.beginPath();
+          this.ctx.moveTo(this.circles[i + 1].x, this.circles[i + 1].y);
+          this.ctx.lineTo(this.circles[i].x, this.circles[i].y);
+          this.ctx.stroke();
+          this.ctx.closePath();
+          this.ctx.beginPath();
+          this.ctx.arc(this.circles[i].x, this.circles[i].y, this.circles[i].radius, 0, Math.PI * 2);
+          this.ctx.closePath();
+          this.ctx.fill();
+        }
+      }
+    }
+  }, {
+    key: "animationTentacles",
+    value: function animationTentacles() {
+      gsap__WEBPACK_IMPORTED_MODULE_0__["default"].to(".first-screen__tentacles", {
+        duration: 1,
+        left: 0
+      });
+    }
+  }, {
+    key: "init",
+    value: function init() {
+      var _this3 = this;
+      gsap__WEBPACK_IMPORTED_MODULE_0__["default"].to(".first-screen__tentacles", {
+        duration: 1,
+        left: 0
+      });
+      setTimeout(function () {
+        _this3.createCanvas();
+        window.addEventListener("resize", _this3.updateCanvasSize);
+        document.getElementById("first-screen").addEventListener("mousemove", function (event) {
+          document.getElementById("cursor").style.opacity = 1;
+          _this3.animate({
+            x: event.pageX,
+            y: event.pageY
+          });
+        });
+        document.getElementById("first-screen").addEventListener("mouseleave", function (event) {
+          document.getElementById("cursor").style.opacity = 0;
+          _this3.animate({
+            x: event.pageX,
+            y: event.pageY
+          });
+        });
+      }, 0);
+    }
+  }]);
+  return FirstScreen;
+}();
+
+// Prloader
+var preloader = /*#__PURE__*/function () {
+  function Prloader() {
+    _classCallCheck(this, Prloader);
+    this.isLoading = false;
+  }
+  _createClass(Prloader, [{
+    key: "init",
+    value: function init() {
+      var _this4 = this;
+      Object(_resize__WEBPACK_IMPORTED_MODULE_2__["default"])([{
+        element: "#preloader",
+        className: "mobile",
+        size: 1023,
+        isLess: false //<=
+      }]);
+
+      setTimeout(function () {
+        var preloader = document.querySelector(".preloader svg animateTransform");
+        preloader.addEventListener("endEvent", function () {
+          var fs = document.querySelector("#first-screen");
+          if (fs) {
+            new firstScreen(true).init();
+            _this4.isPreloaderHidden = true;
+            document.querySelector(".preloader").classList.add("isHidden");
+            return;
+          }
+          _this4.isPreloaderHidden = true;
+          document.querySelector(".preloader").classList.add("isHidden");
+        });
+      }, 0);
+    }
+  }]);
+  return Prloader;
+}();
+var cookiesForm = /*#__PURE__*/function () {
+  function cookiesForm() {
+    _classCallCheck(this, cookiesForm);
+    this.isClosedOnce = false;
+    this.cookieForm = document.querySelector(".cookie-form");
+  }
+  _createClass(cookiesForm, [{
+    key: "checkIfFormClosedOnce",
+    value: function checkIfFormClosedOnce() {
+      if (localStorage.getItem("cookiesFormOnceClosed") == "true") {
+        this.isClosedOnce = true;
+      }
+    }
+  }, {
+    key: "closeForm",
+    value: function closeForm() {
+      localStorage.setItem("cookiesFormOnceClosed", true);
+      this.checkIfFormClosedOnce();
+      this.cookieForm.style.display = "none";
+    }
+  }, {
+    key: "init",
+    value: function init() {
+      var _this5 = this;
+      if (!this.cookieForm) return;
+      this.checkIfFormClosedOnce();
+      if (this.isClosedOnce) {
+        this.closeForm();
+      }
+      document.querySelector(".cookie-form__button").addEventListener("click", function () {
+        _this5.closeForm();
+      });
+    }
+  }]);
+  return cookiesForm;
+}();
+new preloader().init();
+new cookiesForm().init();
+
+/***/ }),
+
+/***/ "./src/js/resize.js":
+/*!**************************!*\
+  !*** ./src/js/resize.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return resizeScreen; });
+function resizeScreen(resizeData) {
+  if (!resizeData.length) return;
+  function resize(element, className, size, isLess) {
+    var el = document.querySelector(element);
+    if (!el) return;
+    if (isLess) {
+      window.innerWidth <= size ? el.classList.add(className) : el.classList.remove(className);
+    } else {
+      window.innerWidth > size ? el.classList.add(className) : el.classList.remove(className);
+    }
+  }
+  window.addEventListener("resize", loopResizeData);
+  function loopResizeData() {
+    resizeData.forEach(function (_ref) {
+      var element = _ref.element,
+        className = _ref.className,
+        size = _ref.size,
+        isLess = _ref.isLess;
+      resize(element, className, size, isLess);
+    });
+  }
+  loopResizeData();
+}
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=global.js.map
